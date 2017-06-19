58dfe4d3c565499e51bf51860b230d36ce34ec66
Alex Orear <alex.orear@gmail.com>
2017-06-19 18:51:47 -0500

    Created Add.cshtml and get/post controllers for Add.schtml

--- src/CheeseMVC/Controllers/CatagoryController.cs
+++ src/CheeseMVC/Controllers/CatagoryController.cs
@@ -1,18 +1,42 @@
-﻿using CheeseMVC.Data;
-using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
 using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CheeseMVC.Controllers
 {
-    public class CatagoryController
+    public class CatagoryController : Controller
     {
         private readonly CheeseDbContext context;
 
         public CatagoryController(CheeseDbContext dbContext)
         {
             context = dbContext;
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCatagory = new CheeseCategory
+                {
+                    Name = addCategoryViewModel.Name
+                };
+
+                context.SaveChanges();
+                return Redirect("/Category");
+            }
+
+            return View(addCategoryViewModel);
         }
     }
 }
